06d35e6d2cbe7d7da6378421282c732383710ea8
Francois-Rene Rideau <fare@tunes.org>
2012-07-23 21:03:15 -0400

    Fix package wrt newer fare-utils.

--- pkgdcl.lisp
+++ pkgdcl.lisp
@@ -3,7 +3,9 @@
 (in-package :cl)
 
 (defpackage :inferior-shell
-  (:use :cl :fare-utils :fare-matcher :xcvb-driver :named-readtables :fare-mop)
+  (:use :cl :fare-utils :fare-matcher :xcvb-driver :named-readtables :fare-mop :alexandria)
+  (:shadowing-import-from :xcvb-driver #:emptyp) ;; also in alexandria
+  (:shadowing-import-from :fare-matcher #:of-type) ;; also in alexandria
   (:export
    #:run #:run/s #:run/ss #:run/lines
    #:simple-command-line-token #:token-string
